dd1f1b86ab155afaf13eda239e0663d0111c7808
aqil <aqilprakoso@corp.bm.co.id>
2023-03-26 11:53:57 +0700

    remove push

--- push.sh
+++ push.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-
-git add Logs_accounts.txt
-git commit -m "log update"
-git push --repo https://revsuzuran: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@github.com/revsuzuran/rev-reward.git 
-
+#nothing
